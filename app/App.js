ced21c0b09159d3ef7a6c341731d7e7b80550693
Jason Gallavin <jasongallavin@gmail.com>
2018-05-31 10:41:25 -0400

    Add react navigation. Remove project files.

--- app/App.js
+++ app/App.js
@@ -1,34 +1,15 @@
 // @flow
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import TodoList from "./components/todo-list";
+import HomeScreen from "./screens/home";
+import AddTodoScreen from "./screens/add-todo";
+import {createStackNavigator} from "react-navigation";
+import CardStackStyleInterpolator from "react-navigation/src/views/StackView/StackViewStyleInterpolator";
 
-export default class App extends React.Component {
-  todos = [
-    {text: 'a todo task'},
-    {text: 'another todo task'},
-    {text: 'a third todo task'},
-    {text: 'a 4th todo task'},
-    ];
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Add todos below</Text>
-        <TodoList todos={this.todos}/>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    margin: 40
-  }
+const App = createStackNavigator({
+  Home: { screen: HomeScreen },
+  AddTodo: { screen: AddTodoScreen },
+}, {
+  transitionConfig: () => ({ screenInterpolator: sceneProps => CardStackStyleInterpolator.forHorizontal(sceneProps) }),
 });
+export default App;
+
---
+++ app/state.js
@@ -0,0 +1,9 @@
+export const state = {
+  todos:  [
+    {text: 'a todo task'},
+    {text: 'another todo task'},
+    {text: 'a third todo task'},
+    {text: 'a 4th todo task'},
+  ],
+  newTodo: { text: ''},
+};
